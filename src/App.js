36767d1424ff7ba7b7d73886d4effdadd06f47d0
Naimi Abdeljalil <naimijalilo22@gmail.com>
2024-03-26 03:47:22 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2>Version 2</h2>
+      <h1 className="title">First React app with c4a</h1>
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
