c8784d06045dfcaf5be80f0eb2777841024176a5
Naimi Abdeljalil <naimijalilo22@gmail.com>
2024-03-26 03:49:50 +0100

    version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h2>Version 2</h2>
+      <h2>Version 2.1</h2>
       <h1 className="title">First React app with c4a</h1>
       <h2>Hello World</h2>
     </div>
